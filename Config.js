5d284f2a58cc7c5be0078e564c974864af52abc3
sadiq12345670 <111084198+sadiq12345670@users.noreply.github.com>
2024-03-22 11:45:33 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
+global.ownernumber = ['09153704384']
+global.ownername = "Blackhammer 👻"//owner name
+global.ytname = "YT: @Kids_gats_moves"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Nigerian "
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Blackhammer 👻' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'Blackhammer🖕'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Blackhammer 👻'
 //console view/theme
-global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.themeemoji = '⚡️'
+global.wm = "BLAZE inc."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://whatsapp.com/channel/0029VaXFS6x5q08fbFbOxI43'
 
 //custom prefix
 global.prefa = ['.']
@@ -38,7 +38,7 @@ global.autoread_status = false //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
+    done: '*here you go!* \n\n*Blackhammer👻*\n\n*⚡️ Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
     prem: '*This feature can be used by premium user only*',
     admin: '*This feature can be used by admin only*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
